270e50ab2f5591b6c8e18c27690bc5b4984ac5ff
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2018-12-29 13:25:46 -0200

    Exibindo erro no input

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -9,6 +9,7 @@ import CompareList from '../../components/CompareList';
 
 export default class Main extends Component {
   state = {
+    repositoryError: false,
     repositoryInput: '',
     repositories: [],
   };
@@ -24,9 +25,10 @@ export default class Main extends Component {
       this.setState({
         repositoryInput: '',
         repositories: [...this.state.repositories, repository],
+        repositoryError: false,
       });
     } catch (err) {
-      console.log(err);
+      this.setState({ repositoryError: true });
     }
   };
 
@@ -35,7 +37,7 @@ export default class Main extends Component {
       <Container>
         <img src={logo} alt="Github Compare" />
 
-        <Form onSubmit={this.handleAddRepository}>
+        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
           <input
             type="text"
             placeholder="usuário/repositório"
