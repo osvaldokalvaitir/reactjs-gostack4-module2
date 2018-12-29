40263e655c5f4407db89255c20aff8ee28535d8d
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2018-12-29 14:41:23 -0200

    Alteração no prop-types

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -52,7 +52,7 @@ CompareList.propTypes = {
       stargazers_count: PropTypes.number,
       forks_count: PropTypes.number,
       open_issues_count: PropTypes.number,
-      pushed_at: PropTypes.string,
+      lastCommit: PropTypes.string,
     }),
   ).isRequired,
 };
